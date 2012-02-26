931ed8745fc8fe17bdfb6ab22703c1524e91efb1
sadasant <sadasant@gmail.com>
2012-02-25 23:53:06 -0430

    better CSJS

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,6 +8,10 @@ window.onload = function() {
     , URL = window.location.href
     , I   = false
     , busy = false
+    , $content = $('#content')
+    , $textarea = $('textarea')
+    , $error = $('#error')
+    , $auto = $('#auto')
 
   bindControls()
 
@@ -30,17 +34,17 @@ window.onload = function() {
 
   function sendPost() {
     var data = {
-      post : $('textarea').val()
+      post : $textarea.val()
     , date : new Date().toString()
     }
-    $('#error').html('loading...')
+    $error.html('loading...')
     $.post(URL+"/post", data, function(data) {
       if (data === 'ok') {
-        $('textarea').val('')
+        $textarea.val('')
         if (!I) loadPosts()
       } else {
         if (data.error) {
-          $('#error').html(data.error)
+          $error.html(data.error)
         }
       }
     })
@@ -49,22 +53,23 @@ window.onload = function() {
   function loadPosts() {
     if (busy) return
     busy = true
-    last = $('.content .post').last().attr('name')
+    last = $content.find('.post').last().attr('name')
     last = last == undefined ? -1 : last*1
     var data = {
         last : last+1
       , I    : I
       }
-    if (!I) $('#error').html('loading...')
+    if (!I) $error.html('loading...')
     $.post(URL+"/load", data, function(data) {
       if (data && (data[0] === undefined || data[0].user)) {
         var i = 0
+        if (data.length && last == -1) $content.html('')
         for (; i < data.length; i++) {
           var post = data[i]
-          $('.content').append('<div class="post '+(USER.id == post.user.id ? 'you' : '')+'" name="'+post.pos+'"><div class="user" data-id="'+post.user.id+'" data-name="'+post.user.name+'">'+post.user.name+' <small class="date">'+post.date.split(' ')[4]+'</small></div><div class="post-post" data-date="'+post.date+'">'+post.post+'</div></div>')
+          $content.append('<div class="post '+(USER.id == post.user.id ? 'you' : '')+'" name="'+post.pos+'"><div class="user" data-id="'+post.user.id+'" data-name="'+post.user.name+'">'+post.user.name+' <small class="date">'+post.date.split(' ')[4]+'</small></div><div class="post-post" data-date="'+post.date+'">'+post.post+'</div></div>')
         }
         last += i+1
-        $('#error').html('')
+        $error.html('')
       } else {
         window.location = "/"
       }
@@ -80,11 +85,11 @@ window.onload = function() {
   function toggleInterval() {
     if (I) {
       I = false
-      $('#auto').val('Auto Load')
+      $auto.val('Auto Load')
     } else {
       I = true
       loadPosts()
-      $('#auto').val('Stop Auto Load')
+      $auto.val('Stop Auto Load')
     }
   }
 
